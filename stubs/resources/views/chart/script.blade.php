1a743767ce90bde632dfee3f0846366c5f9e8884
Jivan Flores <jivan.flores.108@gmail.com>
2023-08-17 14:00:48 -0700

    feat: Convert string passed in dataLabels.formatter to a function

--- stubs/resources/views/chart/script.blade.php
+++ stubs/resources/views/chart/script.blade.php
@@ -1,5 +1,10 @@
 <script>
     var options = @json($chart->getOptions());
+
+    // TODO: Handle other callbacks
+    if (options.dataLabels.formatter) {
+        options.dataLabels.formatter = Function('val', 'opt', options.dataLabels.formatter);
+    }
     var chart = new ApexCharts(document.querySelector("#{!! $chart->id() !!}"), options);
 
     chart.render();
