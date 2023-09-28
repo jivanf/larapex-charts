e85e8de258c61f57b21b2d19bf71585b70f310b5
Jivan Flores <jivan.flores.108@gmail.com>
2023-09-27 17:07:39 -0700

    feat: Handle xaxis and yaxis formatter callbacks

--- stubs/resources/views/chart/script.blade.php
+++ stubs/resources/views/chart/script.blade.php
@@ -1,9 +1,15 @@
 <script>
     var options = @json($chart->getOptions());
 
-    // TODO: Handle other callbacks
-    if (options.dataLabels.formatter) {
+    // TODO: Automatically handle callbacks
+    if (options.dataLabels?.formatter) {
         options.dataLabels.formatter = Function('val', 'opt', options.dataLabels.formatter);
+    }
+    if (options.xaxis?.labels?.formatter) {
+        options.xaxis.labels.formatter = Function('val', 'opt', options.xaxis.labels.formatter);
+    }
+    if (options.yaxis?.labels?.formatter) {
+        options.yaxis.labels.formatter = Function('val', 'opt', options.yaxis.labels.formatter);
     }
     var chart = new ApexCharts(document.querySelector("#{!! $chart->id() !!}"), options);
 
