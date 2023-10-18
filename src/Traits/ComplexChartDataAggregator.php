337a0c9b306d7a3588efb8f0a3292401621e4461
Jivan Flores <jivan.flores.108@gmail.com>
2023-10-17 17:57:11 -0700

    feat: Support markers when adding data

--- src/Traits/ComplexChartDataAggregator.php
+++ src/Traits/ComplexChartDataAggregator.php
@@ -4,16 +4,36 @@ namespace ArielMejiaDev\LarapexCharts\Traits;
 
 trait ComplexChartDataAggregator
 {
-    public function addData(string $name, array $data, string|null $type = null): self
+    public function addData(string $name, array $data, string|null $type = null, array $goals = []): self
     {
+        $defaultGoalOptions = [
+            ...$this->horizontal ? ['strokeWidth' => 5] : ['strokeHeight' => 5],
+            'strokeColor' => '#775DD0',
+        ];
+
         $dataset = json_decode($this->dataset, true);
+
+        foreach ($goals as $goal) {
+            foreach ($defaultGoalOptions as $key => $value) {
+                $goal[$key] ??= $value;
+            }
+        };
 
         $addedData = [
             'name' => $name,
-            'data' => $data,
+            'data' => collect($this->xAxis())
+                ->zip($data, $goals)
+                ->map(
+                    fn($item) => [
+                        'x' => $item[0],
+                        'y' => $item[1],
+                        ...$item[2] !== null ? ['goals' => [$item[2]]] : [],
+                    ],
+                )
+                ->toArray(),
         ];
 
-        if (!is_null($type)) {
+        if ($type !== null) {
             $addedData['type'] = $type;
         }
 
